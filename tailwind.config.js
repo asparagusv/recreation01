cecc0e733fc2121450878788d6e7512aba5efc6a
Takenaka Whistle <sousuke88k@gmail.com>
2022-09-24 22:36:04 +0900

    final

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*html'],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkModeGray: "rgba(32,33,36,1)",
+        searchBarGray: "rgb(48,49,52)",
+        footerGray: "rgb(23,23,23)",
+      },
+    },
   },
   plugins: [],
-}
+};
